339d3e2320c5a4080114846ca0789ce36cf19565
Shantaram K <shantaramk@Mipladmin-5.local>
2019-03-21 15:07:26 +0530

    first commit

---
+++ GoogleLogin/GoogleLoginInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  GoogleLoginInteractor.swift
+//  GoogleLogin
+//
+//  Created by Shantaram K on 21/03/19.
+//  Copyright © 2019 Shantaram K. All rights reserved.
+//
+
+import UIKit
+
+class GoogleLoginInteractor: NSObject {
+
+}
